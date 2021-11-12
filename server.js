27147a43bce1bc443a28c7fb7b2b2555d4e4224c
charwaii <61100546+charwaii@users.noreply.github.com>
2021-11-12 17:15:01 +1100

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,8 +9,6 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -20,6 +19,16 @@ app.use("/api/friend-request", require("./routes/api/friend-request"));
 app.use("/api/notification", require("./routes/api/notification"));
 app.use("/api/conversations", require("./routes/api/conversations"));
 app.use("/api/messages", require("./routes/api/messages"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
