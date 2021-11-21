2bba95e93c24cc769dee1299b6b11d49227a7889
charwaii <61100546+charwaii@users.noreply.github.com>
2021-11-21 15:46:47 +1100

    Fixed Socket/Heroku issue

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
